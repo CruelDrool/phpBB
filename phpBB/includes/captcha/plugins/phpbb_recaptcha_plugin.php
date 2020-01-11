4902a40de69487600d1036fe2f0ad9171a560ca9
CruelDrool <aasmund.sater@gmail.com>
2020-01-11 17:26:37 +0100

    First commit to this branch
    These changes have been tested on a live board, and they work!

--- phpBB/includes/captcha/plugins/phpbb_recaptcha_plugin.php
+++ phpBB/includes/captcha/plugins/phpbb_recaptcha_plugin.php
@@ -27,15 +27,13 @@ if (!class_exists('phpbb_default_captcha'))
 */
 class phpbb_recaptcha extends phpbb_default_captcha
 {
-	var $recaptcha_server = 'http://www.google.com/recaptcha/api';
-	var $recaptcha_server_secure = 'https://www.google.com/recaptcha/api'; // class constants :(
+	var $recaptcha_server = 'http://www.google.com/recaptcha';
+	var $recaptcha_server_secure = 'https://www.google.com/recaptcha'; // class constants :(
 
 	// We are opening a socket to port 80 of this host and send
 	// the POST request asking for verification to the path specified here.
-	var $recaptcha_verify_server = 'www.google.com';
-	var $recaptcha_verify_path = '/recaptcha/api/verify';
-
-	var $challenge;
+	var $recaptcha_verify_url = 'https://www.google.com/recaptcha/api/siteverify';
+	
 	var $response;
 
 	// Constructor
@@ -50,8 +48,7 @@ class phpbb_recaptcha extends phpbb_default_captcha
 
 		$user->add_lang('captcha_recaptcha');
 		parent::init($type);
-		$this->challenge = request_var('recaptcha_challenge_field', '');
-		$this->response = request_var('recaptcha_response_field', '');
+		$this->response = request_var('g-recaptcha-response', '');
 	}
 
 	function is_available()
@@ -207,77 +204,35 @@ class phpbb_recaptcha extends phpbb_default_captcha
 		}
 	}
 
-// Code from here on is based on recaptchalib.php
-/*
- * This is a PHP library that handles calling reCAPTCHA.
- *	- Documentation and latest version
- *		  http://recaptcha.net/plugins/php/
- *	- Get a reCAPTCHA API Key
- *		  http://recaptcha.net/api/getkey
- *	- Discussion group
- *		  http://groups.google.com/group/recaptcha
- *
- * Copyright (c) 2007 reCAPTCHA -- http://recaptcha.net
- * AUTHORS:
- *   Mike Crawford
- *   Ben Maurer
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 	/**
 	* Submits an HTTP POST to a reCAPTCHA server
-	* @param string $host
-	* @param string $path
+	* @param string $url
 	* @param array $data
-	* @param int port
 	* @return array response
 	*/
-	function _recaptcha_http_post($host, $path, $data, $port = 80)
+	function _recaptcha_http_post($url, $params)
 	{
-		$req = $this->_recaptcha_qsencode ($data);
+        $handle = curl_init();
 
-		$http_request  = "POST $path HTTP/1.0\r\n";
-		$http_request .= "Host: $host\r\n";
-		$http_request .= "Content-Type: application/x-www-form-urlencoded;\r\n";
-		$http_request .= "Content-Length: " . strlen($req) . "\r\n";
-		$http_request .= "User-Agent: reCAPTCHA/PHP/phpBB\r\n";
-		$http_request .= "\r\n";
-		$http_request .= $req;
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => http_build_query($params, '', '&'),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded'
+            ),
+            CURLINFO_HEADER_OUT => false,
+            CURLOPT_HEADER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => true
+        );
+        
+        curl_setopt_array($handle, $options);
 
-		$response = '';
-		if (false == ($fs = @fsockopen($host, $port, $errno, $errstr, 10)))
-		{
-			trigger_error('Could not open socket', E_USER_ERROR);
-		}
+        $response = curl_exec($handle);
+        curl_close($handle);
 
-		fwrite($fs, $http_request);
-
-		while (!feof($fs))
-		{
-			// One TCP-IP packet
-			$response .= fgets($fs, 1160);
-		}
-		fclose($fs);
-		$response = explode("\r\n\r\n", $response, 2);
-
-		return $response;
+        return $response;
 	}
 
 	/**
@@ -290,23 +245,22 @@ class phpbb_recaptcha extends phpbb_default_captcha
 		global $config, $user;
 
 		//discard spam submissions
-		if ($this->challenge == null || strlen($this->challenge) == 0 || $this->response == null || strlen($this->response) == 0)
+		if ($this->response == null || strlen($this->response) == 0)
 		{
 			return $user->lang['RECAPTCHA_INCORRECT'];
 		}
 
-		$response = $this->_recaptcha_http_post($this->recaptcha_verify_server, $this->recaptcha_verify_path,
+		$response = $this->_recaptcha_http_post($this->recaptcha_verify_url,
 			array(
-				'privatekey'	=> $config['recaptcha_privkey'],
+				'secret'	    => $config['recaptcha_privkey'],
 				'remoteip'		=> $user->ip,
-				'challenge'		=> $this->challenge,
 				'response'		=> $this->response
 			) + $extra_params
 		);
 
-		$answers = explode("\n", $response[1]);
+		$result = json_decode($response, true);
 
-		if (trim($answers[0]) === 'true')
+		if (isset($result['success']) && $result['success'] == true)
 		{
 			$this->solved = true;
 			return false;
@@ -315,24 +269,6 @@ class phpbb_recaptcha extends phpbb_default_captcha
 		{
 			return $user->lang['RECAPTCHA_INCORRECT'];
 		}
-	}
-
-	/**
-	* Encodes the given data into a query string format
-	* @param $data - array of string elements to be encoded
-	* @return string - encoded request
-	*/
-	function _recaptcha_qsencode($data)
-	{
-		$req = '';
-		foreach ($data as $key => $value)
-		{
-			$req .= $key . '=' . urlencode(stripslashes($value)) . '&';
-		}
-
-		// Cut the last '&'
-		$req = substr($req, 0, strlen($req) - 1);
-		return $req;
 	}
 }
 
