781b27b8095e89457c2648eb8d80574fda25bf9a
CruelDrool <aasmund.sater@gmail.com>
2020-07-12 23:17:24 +0200

    Update memberlist.php
    So far the only thing I've needed to fix for php 7.4.x

--- phpBB/memberlist.php
+++ phpBB/memberlist.php
@@ -499,8 +499,8 @@ switch ($mode)
 
 		$result = $db->sql_query($sql);
 		$row = $db->sql_fetchrow($result);
-		$foe = ($row['foe']) ? true : false;
-		$friend = ($row['friend']) ? true : false;
+		$foe = $row['foe'] ?? false;
+		$friend = $row['friend'] ?? false;
 		$db->sql_freeresult($result);
 
 		if ($config['load_onlinetrack'])
